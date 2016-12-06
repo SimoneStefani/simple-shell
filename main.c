8a7f6463f5cf8d366ab2f8998ae95a5e48a30598
Simone Stefani <s.stefani95@gmail.com>
2016-12-06 17:20:37 +0100

    Implement execution of programs and builtins
    plement execution of builtin and programs

--- main.c
+++ main.c
@@ -1,28 +1,8 @@
-
-int main(int argc, char const **argv)
-{
-    shell_loop();
-
-    return EXIT_SUCCESS;
-}
-
-
-void shell_loop(void)
-{
-    char *line;
-    char **args;
-    int status;
-
-    do {
-        printf("> ");
-        line = shell_read_line();
-        args = shell_split_line(line);
-        status = shell_execute(args);
-
-        free(line);
-        free(args);
-    } while (status);
-}
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 
 #define SHELL_RL_BUFSIZE 1024
@@ -32,6 +12,7 @@ char *shell_read_line(void)
     int bufsize = SHELL_RL_BUFSIZE;
     int position = 0;
     char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
 
     if (!buffer) {
         fprintf(stderr, "shell: Allocation error!\n");
@@ -39,9 +20,11 @@ char *shell_read_line(void)
     }
 
     while (1) {
-        if (c == EOF || c == "\n") {
-            buffer[position] = "\0";
-            return buffer
+        c = getchar();
+
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
         } else {
             buffer[position] = c;
         }
@@ -91,4 +74,101 @@ char **shell_split_line(char *line)
     }
     tokens[position] = NULL;
     return tokens;
+}
+
+int shell_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("shell");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("shell");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+int shell_cd(char **args);
+// int shell_help(char **args);
+// int shell_exit(char **args);
+
+char *builtin_str[] = {
+    "cd"
+    // "help",
+    // "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+    &shell_cd
+    // &shell_help,
+    // &shell_exit
+};
+
+int shell_num_builtins()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int shell_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "shell: Expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("shell");
+        }
+    }
+    return 1;
+}
+
+int shell_execute(char **args)
+{
+    int i;
+
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    for (i = 0; i < shell_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return shell_launch(args);
+}
+
+void shell_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = shell_read_line();
+        args = shell_split_line(line);
+        status = shell_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+int main(int argc, char **argv)
+{
+    shell_loop();
+
+    return EXIT_SUCCESS;
 }
